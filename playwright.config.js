4945849f9bdd71b0db6dc680263d1535e9c24c7c
AlinaTolchenitsyna <102752798+AlinaTolchenitsyna@users.noreply.github.com>
2025-03-09 22:55:32 +0300

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -14,10 +14,6 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
 
-  timeout: 60000, 
-    expect: {
-      timeout: 60000, 
-    },
 
   testDir: './tests',
   /* Run tests in files in parallel */
@@ -34,7 +30,6 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
-    actionTimeout: 30000,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
 
